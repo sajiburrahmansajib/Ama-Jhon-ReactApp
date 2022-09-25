af3c362ec6ab9a07d5e55c203285dc6f53ebfe4c
sajiburrahman <mrahman172152@bscse.uiu.ac.bd>
2022-09-26 00:00:36 +0600

    Card total value calculate

---
+++ src/Components/Cart/Cart.js
@@ -0,0 +1,49 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash, faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import './Cart.css'
+
+
+const Cart = ({ cart }) => {
+    console.log(cart)
+    // const initialValue = 0;
+    // const totalPrice = cart.reduce(
+    //     (previousValue, currentValue) => previousValue + currentValue.price,
+    //     initialValue
+    // );
+    let totalPrice = 0;
+    let shipping = 0;
+    let tax = 0;
+    let grandTotal = 0;
+    for (const product of cart) {
+        totalPrice = totalPrice + product.price;
+        shipping = shipping + product.shipping;
+        tax = tax + product.price * 0.01;
+        grandTotal = totalPrice + shipping + tax;
+    }
+    tax = tax.toFixed(2)
+    grandTotal = parseFloat(grandTotal.toFixed(2))
+    return (
+        <div className='cart'>
+            <h1>Order Summery</h1>
+            <p>Selected Item : {cart.length}</p>
+            <p>Total Price: ${totalPrice}</p>
+            <p>Total Shipping Charge :${shipping}  </p>
+            <p>Tax : ${tax}</p>
+            <h2>Grand Total : ${grandTotal}</h2>
+            <div className='btn-clear'>
+                <button>
+                    <p className='btn-text'>Clear Cart</p>
+                    <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon>
+                </button>
+            </div>
+            <div className='btn-review'>
+                <button>
+                    <p className='btn-text'>Review Orders</p>
+                    <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon>
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Cart;
--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash, faArrowRight } from '@fortawesome/free-solid-svg-icons'
 import { useEffect } from 'react';
 import { useState } from 'react';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
 
-const initialValue = 0;
 const Shop = () => {
     const [products, setProducts] = useState([])
     const [cart, setCart] = useState([])
-    const [totalPrice, setTotalPrice] = useState(0)
-    const [totalShipping, setTotalShipping] = useState(0)
-    const [tax, setTax] = useState(0)
-    const [grandTotal, setGrandTotal] = useState(0)
+
     useEffect(() => {
         fetch('products.json')
             .then(res => res.json())
@@ -22,26 +17,6 @@ const Shop = () => {
     const handleAddToCart = (product) => {
         const newCart = [...cart, product];
         setCart(newCart)
-
-        const totalCost = newCart.reduce(
-            (previousValue, currentValue) => previousValue + currentValue.price,
-            initialValue
-        );
-        setTotalPrice(totalCost)
-        const totaltax = newCart.reduce(
-            (previousValue, currentValue) => previousValue + currentValue.price * 0.05,
-            initialValue
-        );
-        const newtax = Math.ceil(totaltax)
-        setTax(newtax)
-        const totalCostShipping = newCart.reduce(
-            (previousValue, currentValue) => previousValue + currentValue.shipping,
-            initialValue
-        );
-        setTotalShipping(totalCostShipping)
-
-        const total = totalCost + totalCostShipping;
-        setGrandTotal(total)
     }
     return (
         <div className='shop-container'>
@@ -55,24 +30,7 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
-                <h1>Order Summery</h1>
-                <h3>Selected Item : {cart.length}</h3>
-                <h3>Total Price: ${totalPrice}</h3>
-                <h3>Total Shipping Charge : ${totalShipping} </h3>
-                <h3>Tax : ${tax} </h3>
-                <h2>Grand Total : ${grandTotal}</h2>
-                <div className='btn-clear'>
-                    <button>
-                        <p className='btn-text'>Clear Cart</p>
-                        <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon>
-                    </button>
-                </div>
-                <div className='btn-review'>
-                    <button>
-                        <p className='btn-text'>Review Orders</p>
-                        <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon>
-                    </button>
-                </div>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );

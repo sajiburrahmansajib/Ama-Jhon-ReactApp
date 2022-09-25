d4765ae7b5dc2b000b30c18d7c7419c19a9223d9
sajiburrahman <mrahman172152@bscse.uiu.ac.bd>
2022-09-25 22:09:31 +0600

    Simple React Ama Jhon wensite some functionality added

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from '../../images/Logo.svg'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <nav className='headerContainer'>
+            <img src={logo} alt="" />
+            <div>
+                <a href="/shop">Shop</a>
+                <a href="/order">Orders</a>
+                <a href="/inventory">Inventory</a>
+                <a href="/about">About Us</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
---
+++ src/Components/Product/Product.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import './Product.css'
+
+const Product = (props) => {
+    const { name, img, price, seller, ratings } = props.product;
+    const { handleAddToCart } = props
+    return (
+        <div className='products'>
+            <img src={img} alt="" />
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price : ${price}</p>
+                <p><small>Manufacturer : {seller}</small> </p>
+                <p> <small>Rating : {ratings} Star</small> </p>
+            </div>
+            <button onClick={() => handleAddToCart(props.product)} >
+                <p className='btn-text'>Add To Cart</p>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+            </button>
+        </div>
+    );
+};
+
+export default Product;
